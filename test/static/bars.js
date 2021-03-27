7cfd825a00c6b0252a11e89e0ab1d6ff188c22b8
timok1 <timokoster1@gmail.com>
2021-03-27 15:46:54 +0100

    Started work on grouped bar chart

--- test/static/bars.js
+++ test/static/bars.js
@@ -2,48 +2,79 @@
 
 function getSubData(source, target) {
   d3.csv("../../data/averages.csv", function(data) {
+    var data1;
+    var data2;
+    var bool1 = false;
+    var bool2 = false;
     for (let row of data){
       if (row['SOURCE_SUBREDDIT'] === source && row['TARGET_SUBREDDIT'] === target){
-        createBarChart(row)
-        break
+        data1 = row;
+        bool1 = true;
       }
+      if (row['TARGET_SUBREDDIT'] === source && row['SOURCE_SUBREDDIT'] === target){
+        data2 = row;
+        bool2 = true
+      }
+      if (bool1 == true && bool2 == true) {break}
     }
-  })
-}
+
+  createBarChart(data1, data2);
+})}
 
 function initBarChart(source, target) {
   getSubData(source, target);
 }
 
-function createBarChart(data) {
-  var sourceData = data
+function createBarChart(sourceData, targetData) {
+  var data = sourceData
   var margin = {top: 70, right: 0, bottom: 100, left: 90};
   var w = 900 - margin.left;
   var h = 645 - margin.top - margin.bottom;
+  console.log(targetData)
+  subgroups = ['Source', 'Target']
 
-  categories = ['Compound sentiment calculated by VADER', 'LIWC_Swear',
+  categories = ['LIWC_Swear',
        'LIWC_Social', 'LIWC_Family', 'LIWC_Friends', 'LIWC_Humans',
        'LIWC_Affect', 'LIWC_Posemo', 'LIWC_Negemo', 'LIWC_Anx', 'LIWC_Anger',
        'LIWC_Sad',]
   //numberCategories = categories.length;
+  var svgChart;
+
+  var myColor = d3.scaleOrdinal().domain(subgroups)
+    .range(d3.schemeSet3);
+
+  console.log(myColor("Source"))
+
+  fullData = [sourceData, targetData]
 
   // Create svg on first time
   if (d3.select(".barChart")['_groups'][0][0] === null) {
-    var svgChart = d3.select("body").append("svg")
+    svgChart = d3.select("body").append("svg")
                   .attr("class", "barChart")
                   .attr("width", w + margin.left)
                   .attr("height", h + margin.top + margin.bottom)
                 .append("g")
                   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                }
+
+    var maxliwc = 0;
+
+    for (let cat of categories) {
+      if (data[cat] > maxliwc) {maxliwc = data[cat]}
+    }
 
     var yScale = d3.scaleLinear()
-            .domain([0, 1])
+            .domain([0, Math.ceil(maxliwc * 10) / 10 ])
             .range([h, 0]);
-    // Create y-axis
-    var yAxis = d3.axisLeft(yScale);
-    svgChart.append("g")
-            .attr("class", "y-axis")
-            .call(yAxis);
+
+
+
+    var svgChart = d3.select(".barChart")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            //.call(yAxis);
+
+    //svgChart.call(yAxis);
 
     d3.select(".graphTitle").remove();
     svgChart.append("text")
@@ -51,7 +82,7 @@ function createBarChart(data) {
         .attr("x", 0)
         .attr("y", - 18)
         .attr("text-anchor", "start")
-        .text(sourceData["Compound sentiment calculated by VADER"] + " Sentiment");
+        .text(sourceData["Compound sentiment calculated by VADER"] + " Sentiment between " + sourceData['SOURCE_SUBREDDIT'] + " and " + sourceData['TARGET_SUBREDDIT']);
 
     // Add label to y-axis
     svgChart.append("text")
@@ -61,29 +92,71 @@ function createBarChart(data) {
       .attr("text-anchor", "middle")
       .attr("y", - 35)
       .text("LIWC scores");
-  }
 
 
-  // Select chart
-  var svgChart = d3.select(".barChart")
-                  .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    d3.select(".y-axis").remove();
+      // Create y-axis
+    var yAxis = d3.axisLeft(yScale);
+
+    svgChart.append("g")
+            .attr("class", "y-axis")
+            .call(yAxis);
+
+
+
+
 
   var yScale = d3.scaleLinear()
-          .domain([0, 1])
+          .domain([0, Math.ceil(maxliwc * 10) / 10 ])
           .range([h, 0]);
 
   var xScale = d3.scaleBand()
           .domain(categories)
           .range([0, w])
-          .padding(0.1);
+          .padding(0.2);
+
+
+
+  // Create x-axis
+  d3.select(".x-axis").remove();
+  var xAxis = d3.axisBottom(xScale);
+  svgChart.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", "translate(0," + h + ")")
+          .call(xAxis)
+          .selectAll("text")
+          .attr("text-anchor", "end")
+          .attr("y", 3)
+          .attr("x", -7)
+          .attr("transform", "rotate(-30)");
+
+  var xSubgroup = d3.scaleBand()
+   .domain(subgroups)
+   .range([0, xScale.bandwidth()])
+   .padding([0.075])
 
   // Create bars
   d3.selectAll(".bar").remove();
   var rects = svgChart.selectAll("rect")
                 .data(categories)
                 .enter()
-                .append("rect")
+                .append("g")
+                  .attr("transform", function(d) {
+                    console.log(d)
+                    return "translate(" + xScale(d) + ",0)"; })
+                .selectAll("rect")
+                .data(function(d) {
+                  return subgroups.map(function(key) {
+                    var val = (key === "Source") ? fullData[0][d] : fullData[1][d]
+                    return {key: key, value: val}; }); })
+                .enter().append("rect")
+                .attr("x", function(d) {return (d === undefined) ? 0 : xSubgroup(d.key); })
+                .attr("y", function(d) {return (d === undefined) ? 0 :yScale(d.value);})
+                .attr("width", xSubgroup.bandwidth())
+                .attr("height", function(d) {return (d === undefined) ? 0 : h - yScale(d.value);})
+                .attr("fill", function(d) {return myColor(d.key)});
+
+                /*
                 .attr("class","bar")
                 .attr("x", function(d) {
                           return xScale(d);
@@ -96,126 +169,9 @@ function createBarChart(data) {
                       return h - yScale(sourceData[d]);
                 })
                 .attr("fill", "skyblue");
-
-  // Create x-axis
-  d3.select(".x-axis").remove();
-  var xAxis = d3.axisBottom(xScale);
-  svgChart.append("g")
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0," + h + ")")
-          .call(xAxis)
-          .selectAll("text")
-          .attr("text-anchor", "end")
-          .attr("y", 3)
-          .attr("x", -7)
-          .attr("transform", "rotate(-30)");
+                */
 
 
-
-
-}
-
-
-
-  /*
-  var drivers = Object.keys(winners);
-  var nationalDrivers = {};
-  var first = true;
-  // Create new json for just drivers of this country
-  for (let driver of drivers) {
-    if (winners[driver]['country_id'] === countryID){
-      nationalDrivers[driver] = {'victories': winners[driver]['victories']};
-      if (first) {
-        var nationality = winners[driver]['nationality'];
-        first = false;
-      }
-    }
   }
 
-  var nationalDriversNames = Object.keys(nationalDrivers);
-
-  var margin = {top: 50, right: 0, bottom: 70, left: 90};
-  var w = 800 - margin.left;
-  var h = 645 - margin.top - margin.bottom;
-
-  // Create svg on first time
-  if (d3.select(".barChart")['_groups'][0][0] === null) {
-    var svgChart = d3.select("body").append("svg")
-                  .attr("class", "barChart")
-                  .attr("width", w + margin.left)
-                  .attr("height", h + margin.top + margin.bottom)
-                .append("g")
-                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var yScale = d3.scaleLinear()
-            .domain([0, maxWins])
-            .range([h, 0]);
-    // Create y-axis
-    var yAxis = d3.axisLeft(yScale);
-    svgChart.append("g")
-            .attr("class", "y-axis")
-            .call(yAxis);
-
-    // Add label to y-axis
-    svgChart.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("class", "y-axis-label")
-      .attr("x", -(h / 2))
-      .attr("text-anchor", "middle")
-      .attr("y", - 35)
-      .text("Race Victories");
-  }
-
-  // Select chart
-  var svgChart = d3.select(".barChart")
-                  .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var yScale = d3.scaleLinear()
-          .domain([0, maxWins])
-          .range([h, 0]);
-  // Set width of chart depending on amount of drivers
-  if ((nationalDriversNames.length - 1) * 80 < w) {
-    var xRangeMax = nationalDriversNames.length * 80;
-  }
-  else {
-    var xRangeMax = w;
-  }
-  var xScale = d3.scaleBand()
-          .domain(nationalDriversNames)
-          .range([0, xRangeMax])
-          .padding(0.1);
-
-  // Create bars
-  d3.selectAll(".bar").remove();
-  var rects = svgChart.selectAll("rect")
-                .data(nationalDriversNames)
-                .enter()
-                .append("rect")
-                .attr("class","bar")
-                .attr("x", function(d) {
-                          return xScale(d);
-                        })
-                .attr("y", function(d) {
-                      return yScale(nationalDrivers[d].victories);
-                        })
-                .attr("width", xScale.bandwidth())
-                .attr("height", function(d) {
-                      return h - yScale(nationalDrivers[d].victories);
-                })
-                .attr("fill", "skyblue");
-
-  // Create x-axis
-  d3.select(".x-axis").remove();
-  var xAxis = d3.axisBottom(xScale);
-  svgChart.append("g")
-          .attr("class", "x-axis")
-          .attr("transform", "translate(0," + h + ")")
-          .call(xAxis)
-          .selectAll("text")
-          .attr("text-anchor", "end")
-          .attr("y", 3)
-          .attr("x", -7)
-          .attr("transform", "rotate(-30)");
-
-*/
+  svgChart.call(yAxis);
